fc784ffc730fb93ba030f669e634d3126384a019
Ashraful <ashrafulmomin7076@gmail.com>
2025-04-29 13:04:47 +0530

    what is Brypt

---
+++ 12_Understanding Bcrypt/app.mjs
@@ -0,0 +1,11 @@
+//if we go on google and Scarch Bcrypt . there we  can see  bcryptbcrypt(C++)  & bcryptbcrypt.js (javaScript)
+//bcrypt is a Key Derivation Function (KDF, that is used to hash passwords ) & also a Npm package
+
+//step to use 
+  //npm i bcrypt
+
+  import crypto from 'crypto'; //importing crypto module
+  import bcrypt from 'bcrypt'; //importing bcrypt module
+
+  let salt = bcrypt.genSaltSync(10); //generating salt with 10 rounds of salting
+   console.log(salt)
